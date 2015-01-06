b461fd8416895446f914377929cd6d0f3bf5c8aa
yeagle <dominik.wabersich@gmail.com>
2015-01-06 17:34:10 +0100

    added logdensity function

--- R/funcs.R
+++ R/funcs.R
@@ -1,17 +1,18 @@
 # Amoroso distribution function in R by EJ
-damoroso_r <- function(x, a, theta, alpha, beta)
+damoroso_r <- function(x, a, theta, alpha, beta, give_log=FALSE)
 {
   d <- 1 / gamma(alpha) * abs(beta / theta) * ( (x - a) / theta) ^ (alpha * beta - 1) *
       exp(- (((x - a) / theta) ^ beta) )
+  if (give_log) d <- log(d)
   return(d)
 }
 
 # Amoroso distribution function ported to C code
-damoroso <- function(x, a, theta, alpha, beta)
+damoroso <- function(x, a, theta, alpha, beta, give_log=FALSE)
 {
   d <- vector("double", length=length(x))
   for (i in 1:length(x)) {
-    d[i] <- .Call(damoroso_c, x[i], a, theta, alpha, beta)
+    d[i] <- .Call(damoroso_c, x[i], a, theta, alpha, beta, give_log)
   }
   return(d)
 }
--- src/damoroso.c
+++ src/damoroso.c
@@ -13,11 +13,26 @@ double density_amoroso(double x, double a, double theta, double alpha, double be
   return d;
 }
 
-SEXP  damoroso(SEXP x, SEXP a, SEXP theta, SEXP alpha, SEXP beta) {
+double logdensity_amoroso(double x, double a, double theta, double alpha, double beta)
+{
+  double d;
+  
+  d = -log(gammafn(alpha)) + log(fabs(beta/theta)) + 
+      ((alpha*beta)-1)*log((x-a)/theta) + 
+      ( -pow(((x-a)/theta), beta) );
+
+
+  return d;
+}
+
+SEXP  damoroso(SEXP x, SEXP a, SEXP theta, SEXP alpha, SEXP beta, SEXP give_log) {
   double d;
   SEXP value;
     
-  d = density_amoroso(REAL(x)[0], REAL(a)[0], REAL(theta)[0], REAL(alpha)[0], REAL(beta)[0]);
+  if(LOGICAL(give_log)[0]) 
+    d = logdensity_amoroso(REAL(x)[0], REAL(a)[0], REAL(theta)[0], REAL(alpha)[0], REAL(beta)[0]);
+  else
+    d = density_amoroso(REAL(x)[0], REAL(a)[0], REAL(theta)[0], REAL(alpha)[0], REAL(beta)[0]);
     
   PROTECT(value = allocVector(REALSXP, 1));
   REAL(value)[0] = d;
